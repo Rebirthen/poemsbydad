b3363d5cfc162693e4f5d761b310a263301c963a
Diana Temirkhan <diana.temirkhan@icloud.com>
2022-05-26 01:39:39 +0600

    2

--- src/main/admin/Admin.js
+++ src/main/admin/Admin.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './converter.css'
 import DB from "../../db.json";
+import { Input } from 'antd';
 
+const { TextArea } = Input;
 export class Admin extends React.Component {
     constructor() {
         super();
@@ -22,74 +24,92 @@ export class Admin extends React.Component {
 
     handleFormSubmit = (event) => {
         event.preventDefault();
-        let {db} = this.state;
-        let poems = this.state.db[this.state.result.category]['poems'];
-        poems.push(this.state.result);
+        let { db,result } = this.state;
+        let poems = this.state.db[result.category]['poems'];
+        poems.push(result);
         console.log(poems);
 
-        db[this.state.result.category]['poems'] = poems;
-       this.setState({db}, () => console.log("dbbbbbbb",this.state.db));
-       
+        db[result.category]['poems'] = poems;
+        this.setState({ db , result: {
+            name: "",
+            citate: "",
+            citateAuthor: "",
+            imageUrl: "",
+            text: "",
+            footer: "",
+            category: 0
+        }});
+
         
+
+
     }
 
     onChange = (event) => {
         let { name, value } = event.target;
         console.log(name, value);
-        this.setState({ result: {...this.state.result, [name]: value} })
-    
+        this.setState({ result: { ...this.state.result, [name]: value } })
+
 
     }
 
     render() {
+        let {result} =this.state;
         return (
             <section className="contact-form" >
-                <h1>Send Me a Message</h1>
+                <h1>Create a poem</h1>
                 <p>Use this handy contact form to get in touch with me.</p>
 
-                <form>
-                <div className="input-group">
+                    <div className="input-group">
                         <label >category</label>
-                        <select id="category"  name="category" type="citate" onChange={this.onChange} >
+                        <select id="category" name="category" type="citate" value={result.category} onChange={this.onChange} >
                             {console.log(this.state.db.length)}
                             {this.state.db.map(e => <option key={e.key} value={e.key}>{e.label}</option>)}
-                            </select>
-                    </div> 
+                        </select>
+                    </div>
 
                     <div className="input-group">
                         <label >name</label>
-                        <input id="name" name="name" type="text" onChange={this.onChange} />
-                    </div>
-
-                    <div className="input-group">
-                        <label >citate</label>
-                        <input id="citate" name="citate" type="citate" onChange={this.onChange} />
-                    </div>
-
-                    
-
-                    <div className="input-group">
-                        <label >citateAuthor</label>
-                        <input id="citateAuthor" name="citateAuthor" type="citateAuthor" onChange={this.onChange} />
-                    </div>
-
-                    <div className="input-group">
-                        <label >imageUrl</label>
-                        <input id="imageUrl" name="imageUrl" type="imageUrl" onChange={this.onChange} />
+                        <TextArea id="name" name="name" type="text" value={result.name}  onChange={this.onChange} />
                     </div>
 
                     <div className="input-group">
                         <label >text</label>
-                        <input id="text" name="text" type="text" onChange={this.onChange} />
+                        <TextArea rows={4} id="text" name="text" type="text" value={result.text}  onChange={this.onChange} />
                     </div>
                     <div className="input-group">
                         <label >footer</label>
-                        <input id="footer" name="footer" type="footer" onChange={this.onChange} />
+                        <TextArea rows={4} id="footer" name="footer" type="footer" value={result.footer}  onChange={this.onChange} />
                     </div>
-                    <button type="submit" onClick={this.handleFormSubmit}>Send It!</button>
-                </form>
 
+                    <button onClick={this.handleFormSubmit}>Send It!</button>
+
+                    <div className="input-group">
+                        <label >citate</label>
+                        <TextArea rows={4} id="citate" name="citate" type="citate" value={result.citate} onChange={this.onChange} />
+                    </div>
+
+
+
+                    <div className="input-group">
+                        <label >citateAuthor</label>
+                        <input id="citateAuthor" name="citateAuthor" type="citateAuthor" value={result.citateAuthor} onChange={this.onChange} />
+                    </div>
+
+                    <div className="input-group">
+                        <label >imageUrl</label>
+                        <input id="imageUrl" name="imageUrl" type="imageUrl" value={result.imageUrl}  onChange={this.onChange} />
+                    </div>
+
+                  
+                    <button onClick={this.handleFormSubmit}>Send It!</button>
+                <button
+                    onClick={() => navigator.clipboard.writeText(JSON.stringify(this.state.db))}
+                >
+                    Copy
+                </button>
                 <p>{JSON.stringify(this.state.db)} </p>
+              
             </section>
         )
     }
